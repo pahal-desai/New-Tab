66f090c4c3c3f15f9b369aa080ef4ece8676f35e
pahal-desai <desaipahal64@gmail.com>
2026-09-16 20:10:39 +0530

    trying to add weather widget

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ setInterval(() => {
     });
     document.querySelector('.time').textContent = time;
 }, 1000);
+async function getWeather() {
+    const city = document.getElementById("city").value;
+
+    const geo = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`
+    ).then(r => r.json());
+
+    const lat = geo.results[0].latitude;
+    const lon = geo.results[0].longitude;
+
+    const weather = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m`
+    ).then(r => r.json());
+
+    document.getElementById("temp").textContent =
+        weather.current.temperature_2m + "°C";
+}
